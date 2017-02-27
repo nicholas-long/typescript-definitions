9ca9a0bd417a565c65b3221b35dfcff5b0e7bc8c
nicholas-long <nicholas.eden.long@gmail.com>
2017-02-27 12:17:07 -0600

    added blob cache for file uploads; formatting
    allow interfacing with tinymce.activeEditor.editorUpload.blobCache for
    tinyMCE file uploading code

--- tinymce.d.ts
+++ tinymce.d.ts
@@ -1,27 +1,41 @@
-// incomplete definitions for http://www.tinymce.com
+﻿// incomplete definitions for http://www.tinymce.com
 
 interface TinyMceObservable {
-    off: (name?:string, callback?:Function) => Object
-    on: (name:string, callback:Function) => Object
-    fire: (name:string, args?:Object, bubble?:Boolean) => Event
+	off: (name?: string, callback?: Function) => Object
+	on: (name: string, callback: Function) => Object
+	fire: (name: string, args?: Object, bubble?: Boolean) => Event
+}
+
+interface TinyMceBlobInfo {
+	blobUri: () => string
+}
+
+interface TinyMceBlobCache {
+	create: (string, File) => TinyMceBlobInfo
+	add: (TinyMceBlobInfo) => void
+}
+
+interface TinyMceEditorUpload {
+	blobCache: TinyMceBlobCache
 }
 
 interface TinyMceEditor extends TinyMceObservable {
-    destroy: (automatic:boolean) => void
-    remove: () => void
-    hide: () => void
-    show: () => void
-    getContent: (args?:Object) => string
-    setContent: (content:string, args?:Object) => string
-    focus: (skip_focus?:Boolean) => void
-    undoManager: TinyMceUndoManager
-    settings: Object
+	destroy: (automatic: boolean) => void
+	remove: () => void
+	hide: () => void
+	show: () => void
+	getContent: (args?: Object) => string
+	setContent: (content: string, args?: Object) => string
+	focus: (skip_focus?: Boolean) => void
+	undoManager: TinyMceUndoManager
+	settings: Object
+	editorUpload: TinyMceEditorUpload
 }
 
 interface TinyMceUndoManager {
-    undo: () => Object
-    clear: () => void
-    hasUndo: () => Boolean
+	undo: () => Object
+	clear: () => void
+	hasUndo: () => Boolean
 }
 
 interface TinyMceEvent {
@@ -29,11 +43,11 @@ interface TinyMceEvent {
 }
 
 interface TinyMceStatic extends TinyMceObservable {
-    init: (settings:Object) => void;
-    execCommand: (c:string, u:Boolean, v:string) => Boolean;
-    activeEditor: TinyMceEditor;
-    get: (id:String) => TinyMceEditor;
-    remove: (id:String) => void;
+	init: (settings: Object) => void;
+	execCommand: (c: string, u: Boolean, v: string) => Boolean;
+	activeEditor: TinyMceEditor;
+	get: (id: String) => TinyMceEditor;
+	remove: (id: String) => void;
 }
 
-declare var tinymce:TinyMceStatic;
+declare var tinymce: TinyMceStatic;
